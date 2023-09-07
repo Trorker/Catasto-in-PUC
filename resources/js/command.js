abcb8f3742d1e0662046df45a2d6e662aa34d982
Ruslan Dzyuba <ruslick.ua@gmail.com>
2023-09-07 12:58:21 +0200

    Update command.js

--- resources/js/command.js
+++ resources/js/command.js
@@ -37,9 +37,13 @@ const SmoothScrolling = () => {
 
 const feedback = (e) => {
     e.preventDefault();
-    console.log(e);
-    window.open("mailto:ruslan.dzyuba@e-distribuzione.com");
-    e.target.action = "mailto:ruslan.dzyuba@e-distribuzione.com?subject=" + oggetto + "&body=" + messaggio;
+
+    var oggetto = document.getElementById("subject").value;
+    var messaggio = document.getElementById("body").value;
+
+    const mailto = `mailto:ruslan.dzyuba@e-distribuzione.com?subject=${oggetto}&body=${messaggio}`;
+
+    window.open(mailto);
 }
 
 window.onload = () => {
