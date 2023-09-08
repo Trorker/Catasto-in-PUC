525edba55ed0573b89a15ff5e51f0b2af4a37355
Dzyuba Ruslan (Grids IT EMR) <ruslick.ua@gmail.com>
2023-09-08 08:31:00 +0200

    Update Catasto_in_PUC.user.js

--- script/Catasto_in_PUC.user.js
+++ script/Catasto_in_PUC.user.js
@@ -4,15 +4,15 @@
 // @version     1.1.0
 // @description Aggiunge il catasto nelle mappe PUC
 // @author      Ruslan Dzyuba
-// @downloadURL https://github.com/Trorker/Catasto-in-PUC/script/Catasto_in_PUC.user.js
-// @updateURL   https://github.com/Trorker/Catasto-in-PUC/script/Catasto_in_PUC.user.js
+// @downloadURL https://trorker.github.io/Catasto-in-PUC/script/Catasto_in_PUC.user.js
+// @updateURL   https://trorker.github.io/Catasto-in-PUC/script/Catasto_in_PUC.user.js
 // @match       https://puc-ita.enelint.global/EditRete/*
 // @match       https://puc-ita.enelint.global/p3/#/P3?*
-// @icon        https://github.com/Trorker/Catasto-in-PUC/resource/img/Catasto_in_PUC.logo.svg
+// @icon        https://trorker.github.io/Catasto-in-PUC/resource/img/Catasto_in_PUC.logo.svg
 // @require     https://cdn.jsdelivr.net/npm/sweetalert2@11
 // @require     https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.5.0/proj4.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/proj4leaflet/1.0.2/proj4leaflet.js
-// @require     https://github.com/Trorker/Catasto-in-PUC/resources/js/library/vComparator.js
+// @require     https://trorker.github.io/Catasto-in-PUC/resources/js/library/vComparator.js
 // ==/UserScript==
 
 (function () {
