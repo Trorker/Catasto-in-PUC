a6c2ca32f0f20fb3163dab64a0bd589da0136de4
Dzyuba Ruslan (Grids IT EMR) <ruslick.ua@gmail.com>
2023-11-09 17:28:16 +0100

    add Grid_People_Awards_2023

--- script/Catasto_in_PUC.user.js
+++ script/Catasto_in_PUC.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Catasto in PUC
 // @namespace   https://github.com/Trorker/Catasto-in-PUC
-// @version     1.1.1 release
+// @version     1.1.2 release
 // @description Aggiunge il catasto nelle mappe PUC
 // @author      Ruslan Dzyuba
 // @downloadURL https://trorker.github.io/Catasto-in-PUC/script/Catasto_in_PUC.user.js
@@ -32,13 +32,13 @@
                         .swal2-container {-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}
                         .swal2-container a {color: #ff0062 }
                         .swal2-container .link {color: #ff0062; cursor: pointer; }
+                        .swal2-container .swal2-popup { background: #4b4e5d; color: #ffffff; }
+                        .swal2-container .swal2-footer { border-top: 1px solid #212125; }
                         .swal2-close { box-shadow: none !important; }
                         .swal2-close:hover { color: #ff0062; }
                         .swal2-popup { font-size: larger; }
                         .swal2-styled.swal2-confirm { background-color: #ff0062; }
                         img.swal2-image { border-radius: 50%; }
-                        div:where(.swal2-container) div:where(.swal2-footer) { border-top: 1px solid #212125; }
-                        div:where(.swal2-container) div:where(.swal2-popup) { background: #4b4e5d; color: #ffffff; }
 
                         .drop .text,.or{font-weight:500}.drop-container{padding:1.5rem}.drop{border-radius:10px;border:3px dashed #aaa;display:grid;width:100%;place-content:center;padding:2rem;box-sizing:border-box;display:grid;place-items:center}.file-input{display:none !important}.active{border:3px solid grey;background:#ff0f6440}.drop .text{text-align:center;margin-top:1rem;color:white}.progress{background:#ff0f64;width:0%;border-radius:10px;transition:.2s;height:20px}.drop label{background:#ff0f64;padding:.7rem 1.8rem;border-radius:5px;color:white;cursor:pointer}.line{width:80px;height:1px;background:#949494}.or-con{display:flex;align-items:center;margin:.5rem}.or{margin:0 1rem;color:white}
                         .switch::after,.switch::before{content:"";display:inline-flex;position:absolute;border-radius:1em}input.switch{font-size:1.25em;height:.7em;display:inline-flex;align-items:center;width:1.65em;position:relative;margin:.3em 0;cursor:pointer}.switch::before{height:.8em;width:1.4em;padding:0 .2em;background:#bdb9a6;box-shadow:inset 0 .1em .3em rgba(0,0,0,.3);-webkit-transition:.3s;-moz-transition:.3s;transition:.3s}.switch::after{height:1em;width:1em;background:#fff;box-shadow:0 .1em .3em rgba(0,0,0,.3);-webkit-transition:.3s;-moz-transition:.3s;transition:.3s}input.switch:checked::after{-webkit-transform:translateX(80%);-moz-transform:translateX(80%);transform:translateX(80%)}input.switch:checked::before{background:#ff0062}input.switch:disabled::after,input.switch:disabled::before{background:#ccc;cursor:not-allowed}
@@ -64,6 +64,26 @@
         showConfirmButton: false,
         timer: 5000,
         timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', window.Swal.stopTimer)
+            toast.addEventListener('mouseleave', window.Swal.resumeTimer)
+        }
+    });
+
+      window.News = window.Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        //showConfirmButton: false,
+        timer: 10000,
+        timerProgressBar: true,
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: '<b style="color: #ff0062">Scopri</b>',
+        denyButtonText: 'Nascondi',
+        cancelButtonText: 'Cancella',
+        confirmButtonColor: "#4b4e5d",
+        denyButtonColor: "#4b4e5d",
+        cancelButtonColor: "#4b4e5d",
         didOpen: (toast) => {
             toast.addEventListener('mouseenter', window.Swal.stopTimer)
             toast.addEventListener('mouseleave', window.Swal.resumeTimer)
@@ -405,6 +425,25 @@
                         });
 
                         global_map.tileOverlays.gisOverlay.proprietary_tileOverlay.bringToFront(); //BETA add in 1.0.6
+
+                        /*if ( !localStorage.getItem("isDismissed_News_Grid_People_Awards_2023") ) {
+                          localStorage.setItem("isDismissed_News_Grid_People_Awards_2023", false);
+                        }*/
+
+                        if (new Date().getTime() <= new Date("2023-11-10T23:59:59.000Z").getTime() && !localStorage.getItem("isDismissed_News_Grid_People_Awards_2023")) {
+                            window.News.fire({
+                                icon: 'info',
+                                title: 'il progetto “Mappe catastali in PUC” è stato candidato sulla piattaforma WeGrid all’interno di "Grid People Awards 2023"'
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    window.open("https://wegrid.enel.com/it/component/zoo/item/nuovo-assunto-nuove-idee-nuova-mappa-74a8f3eda0", '_blank').focus();
+                                }
+                                if (result.isDismissed) {
+                                    localStorage.setItem("isDismissed_News_Grid_People_Awards_2023", true);
+                                };
+                            });
+                        }
+
                     }
                 });
 
