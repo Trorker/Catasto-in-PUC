886e79fd531b0cd8f09574fc7cd00114c844a296
Dzyuba Ruslan (Grids IT EMR) <ruslick.ua@gmail.com>
2024-03-18 10:33:31 +0100

    Update [Sviluppo] Catasto_in_PUC.user.js

--- script/[Sviluppo] Catasto_in_PUC.user.js
+++ script/[Sviluppo] Catasto_in_PUC.user.js
@@ -8,6 +8,7 @@
 // @updateURL   https://trorker.github.io/Catasto-in-PUC/script/Catasto_in_PUC.user.js
 // @match       https://*u*-it*.*ne*i*t.*/E*i*R*te/*
 // @match       https://*u*-it*.*ne*i*t.*/p3/#/P3?*
+// @match       https://*u*-it*.*ne*i*t.*/*U*A*e*a/#/AT?*
 // @icon        https://trorker.github.io/Catasto-in-PUC/resources/img/Catasto_in_PUC.logo.svg
 // @require     https://cdn.jsdelivr.net/npm/sweetalert2@11
 // @require     https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.5.0/proj4.js
